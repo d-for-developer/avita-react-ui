9fbf51d2d8ef4041e0c6039fd43a0ee12b7ecbc5
avitaind <avitaind@gmail.com>
2022-05-25 10:15:28 +0530

    User dashboard product detail page completed

--- src/user/products/ShowProduct.js
+++ src/user/products/ShowProduct.js
@@ -32,8 +32,9 @@ export default function ShowProduct() {
           </div>
 
           <div className='col-md-7'>
+            <h4 className='mb-4 text-center mb-5 mt-5'>Other Information</h4>
+
             <div class="accordion" id="accordionExample">
-            <h4 className='mb-4 text-center mb-4'>Other Information</h4>
 
               <div class="accordion-item">
                 <h2 class="accordion-header" id="headingOne">
@@ -58,70 +59,72 @@ export default function ShowProduct() {
                     <h6>Serial No. : </h6>
                     <small>B09V73PJB8</small>
                     <hr />
+                    <h6>Product Name : </h6>
+                    <small>AVITA SATUS ULTIMUS S111 NU14A1INC43PN-MB 14.1 FHD (35.81cms) Laptop (Intel Celeron N4020/4GB/128GB SSD/FHD Display/Windows 11 Home/ Intel UHD Graphics), Matt Black </small>
+
+                  </div>
+                </div>
+              </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                    <h5 className='mb-4'>Order Details</h5>
+                  </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    <h6>Order Date : </h6>
+                    <small>22<sup>nd</sup> Feb, 2022</small>
+                    <hr />
+                    <h6>Amount : </h6>
+                    <small>INR 21,490/-</small>
+                    <hr />
+                    <h6>Discount (if any) : </h6>
+                    <small>5%</small>
+                    <hr />
+                    <h6>Invoice No. : </h6>
+                    <small>#1031</small>
+                    <hr />
+                    <h6>Payment Method : </h6>
+                    <small>Online / Credit Card</small>
+                    <hr />
                     <h6>Reseller Name : </h6>
                     <small>Amazon</small>
                   </div>
                 </div>
               </div>
-              <div class="accordion-item">
-              <h2 class="accordion-header" id="headingTwo">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                  <h5 className='mb-4'>Order Details</h5>
-                </button>
-              </h2>
-              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <h6>Type : </h6>
-                  <small>Notebook</small>
-                  <hr />
-                  <h6>Series : </h6>
-                  <small>Ultimus</small>
-                  <hr />
-                  <h6>Model : </h6>
-                  <small>NU14A</small>
-                  <hr />
-                  <h6>Product No. : </h6>
-                  <small>NU14A1INC43PN-MB</small>
-                  <hr />
-                  <h6>Serial No. : </h6>
-                  <small>B09V73PJB8</small>
-                  <hr />
-                  <h6>Reseller Name : </h6>
-                  <small>Amazon</small>
-                </div>
-              </div>
-            </div>
-  
-              <div class="accordion-item">
-              <h2 class="accordion-header" id="headingThree">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
-                  <h5 className='mb-4'>Warranty Details</h5>
-                </button>
-              </h2>
-              <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <h6>Type : </h6>
-                  <small>Notebook</small>
-                  <hr />
-                  <h6>Series : </h6>
-                  <small>Ultimus</small>
-                  <hr />
-                  <h6>Model : </h6>
-                  <small>NU14A</small>
-                  <hr />
-                  <h6>Product No. : </h6>
-                  <small>NU14A1INC43PN-MB</small>
-                  <hr />
-                  <h6>Serial No. : </h6>
-                  <small>B09V73PJB8</small>
-                  <hr />
-                  <h6>Reseller Name : </h6>
-                  <small>Amazon</small>
-                </div>
-              </div>
-            </div>
 
-      
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                    <h5 className='mb-4'>Warranty Details</h5>
+                  </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    <h6>Registered on : </h6>
+                    <small>22<sup>nd</sup> Feb, 2022</small>
+                    <hr />
+                    <h6>Expiry on : </h6>
+                    <small>21<sup>st</sup> Aug, 2023</small>
+                    <hr />
+                    <h6>Duration : </h6>
+                    <small>18 Months</small>
+                    <hr />
+                    <h6>Price : </h6>
+                    <small>INR 1099/-</small>
+                    <hr />
+                    <h6>Extended Warranty (if any) : </h6>
+                    <small>No</small>
+                    <hr />
+                  
+                    <h6>Accidental Damage Protection (if any) : </h6>
+                    <small>No</small>
+                  </div>
+                </div>
+              </div>
+
+
 
             </div>
           </div>
