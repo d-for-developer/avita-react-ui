94b04198c0d7ed50d862b263cfb29ed536aee06f
avitaind <avitaind@gmail.com>
2022-06-14 11:50:30 +0530

    mobile view sidebar completed

--- src/components/layouts/SideDrawer.js
+++ src/components/layouts/SideDrawer.js
@@ -1,33 +1,56 @@
-import React, {useState} from 'react';
-import { Drawer, List, ListItemIcon, ListItemText, ListItemButton, IconButton } from '@mui/material';
+import React, { useState } from 'react';
+import { Drawer, List, Box, ListItemIcon, ListItemText, ListItemButton, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import Logo from '../../img/Logo.png'
+
 
 const SideDrawer = () => {
 
     const PAGES = ["Products", "News", "Where to Buy", "Service", "Sales Enquiry", "Contact"];
-    
+
     const [openDrawer, setOpenDrawer] = useState(false);
 
     return (
         <React.Fragment>
-            <Drawer anchor='top' open={openDrawer} onClose={()=> setOpenDrawer(false)} >
-                <List sx={{ marginTop: '2%'}} >
-                {
-                    PAGES.map((pages, index) => (
-                        <ListItemButton key={index}>
-                        <ListItemIcon>
-                            <ListItemText>{pages}</ListItemText>
-                        </ListItemIcon>
-                     </ListItemButton>   
-                    ))
-                }
-                     
+            <Drawer anchor='top' open={openDrawer} onClose={() => setOpenDrawer(false)}>
+                <IconButton onClick={() => setOpenDrawer(!openDrawer)} sx={{ color: "#692c90", right: '40%', top: '15px' }} >
+                    <CloseOutlinedIcon />
+                </IconButton>
+                <Box
+                    component="img"
+                    sx={{
+                        height: 21,
+                        position: 'absolute',
+                        left: '50%',
+                        top: '5%',
+                        transform: 'translate(-50%, -50%)',
+                        zIndex: '1'
+                    }}
+                    alt="Your logo."
+                    src={Logo}
+                />
+                <List sx={{ marginLeft: '2%', marginTop: '15%', height: '100vh' }} >
+
+                    {
+                        PAGES.map((pages, index) => (
+                            <ListItemButton key={index}>
+                                <ListItemIcon>
+                                    <ListItemText>{pages}<hr /></ListItemText>
+                                </ListItemIcon>
+
+                            </ListItemButton>
+
+
+                        ))
+                    }
+
                 </List>
             </Drawer>
-            <IconButton onClick={()=> setOpenDrawer(!openDrawer)} sx={{ color: "#692c90", marginLeft: 'auto'}} >
+            <IconButton onClick={() => setOpenDrawer(!openDrawer)} sx={{ color: "#692c90", marginRight: 'auto' }} >
                 <MenuIcon />
             </IconButton>
-        
+
         </React.Fragment>
     )
 }
