916a26074a007e298e9d3c2d5b746cef899f2e06
avitaind <avitaind@gmail.com>
2022-07-26 18:27:36 +0530

    homeBadge draft 1

--- src/components/grid/BadgeHome.js
+++ src/components/grid/BadgeHome.js
@@ -2,20 +2,89 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import CountUp from 'react-countup';
+import Typography from '@mui/material/Typography';
+
 
 
 const BadgeHome = () => {
   return (
     <Container>
 
-    <Row>
-      <Col>1 of 4</Col>
-      <Col>2 of 4</Col>
-      <Col>3 of 4</Col>
-      <Col>4 of 4</Col>
+      <Row>
+        <Col>
+        <Typography variant="h4" component="div" gutterBottom>
+        INDIA'S NO.
+      </Typography>
+          <CountUp
+            start={100}
+            end={1}
+            duration={2.75}
+            separator=" "
+            className='homeBadge'            
+          >
+           
+          </CountUp>
+          <Typography variant="p" component="div" gutterBottom>
+        WEARABLE WATCH BRAND
+      </Typography>
+         </Col>
+         <Col>
+         <Typography variant="h4" component="div" gutterBottom>
+        APAC NO.
+      </Typography>
+          <CountUp
+            start={100}
+            end={4}
+            duration={2.75}
+            separator=" "
+            className='homeBadge'            
 
-    </Row>
-  </Container>
+          >
+           
+          </CountUp>
+          <Typography variant="p" component="div" gutterBottom>
+        WEARABLE WATCH BRAND
+      </Typography>
+         </Col>
+         <Col>
+         <Typography variant="h4" component="div" gutterBottom>
+        GLOBALLY NO.
+      </Typography>
+          <CountUp
+            start={100}
+            end={5}
+            duration={2.75}
+            separator=" "
+            className='homeBadge'            
+
+          >
+           
+          </CountUp>
+          <Typography variant="p" component="div" gutterBottom>
+        WEARABLE WATCH BRAND
+      </Typography>
+         </Col>
+         <Col>
+         <Typography variant="h4" component="div" gutterBottom>
+        AVAILABLE INTO
+      </Typography>
+          <CountUp
+            start={100}
+            end={10}
+            duration={2.75}
+            separator=" "
+            className='homeBadge'            
+
+          >
+           
+          </CountUp>
+          <Typography variant="p" component="div" gutterBottom>
+        WEARABLE WATCH BRAND
+      </Typography>
+         </Col>
+      </Row>
+    </Container>
 
   )
 }
